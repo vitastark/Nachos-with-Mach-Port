44c12d478288399e32a7f95f15a3b2729d55622e
Haiyang <40311161+vitastark@users.noreply.github.com>
2018-11-25 21:17:07 -0500

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/nachos.dir/DependInfo.cmake
@@ -0,0 +1,97 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/qingqingluan/Desktop/nachos/code/test/add.c" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/test/add.c.o"
+  "/Users/qingqingluan/Desktop/nachos/code/test/halt.c" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/test/halt.c.o"
+  "/Users/qingqingluan/Desktop/nachos/code/test/matmult.c" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/test/matmult.c.o"
+  "/Users/qingqingluan/Desktop/nachos/code/test/segments.c" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/test/segments.c.o"
+  "/Users/qingqingluan/Desktop/nachos/code/test/shell.c" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/test/shell.c.o"
+  "/Users/qingqingluan/Desktop/nachos/code/test/sort.c" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/test/sort.c.o"
+  "/Users/qingqingluan/Desktop/nachos/coff2noff/coff2noff.c" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/coff2noff/coff2noff.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../c++example"
+  "../code"
+  "../code/build.cygwin"
+  "../code/build.linux"
+  "../code/build.macosx"
+  "../code/filesys"
+  "../code/lib"
+  "../code/machine"
+  "../code/network"
+  "../code/test"
+  "../code/threads"
+  "../code/userprog"
+  "../coff2noff"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/qingqingluan/Desktop/nachos/c++example/inheritstack.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/c++example/inheritstack.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/c++example/list.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/c++example/list.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/c++example/stack.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/c++example/stack.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/c++example/templatestack.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/c++example/templatestack.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/filesys/directory.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/filesys/directory.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/filesys/filehdr.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/filesys/filehdr.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/filesys/filesys.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/filesys/filesys.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/filesys/openfile.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/filesys/openfile.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/filesys/pbitmap.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/filesys/pbitmap.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/filesys/synchdisk.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/filesys/synchdisk.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/lib/bitmap.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/lib/bitmap.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/lib/debug.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/lib/debug.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/lib/hash.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/lib/hash.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/lib/libtest.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/lib/libtest.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/lib/list.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/lib/list.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/lib/sysdep.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/lib/sysdep.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/machine/console.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/machine/console.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/machine/disk.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/machine/disk.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/machine/interrupt.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/machine/interrupt.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/machine/machine.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/machine/machine.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/machine/mipssim.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/machine/mipssim.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/machine/network.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/machine/network.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/machine/stats.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/machine/stats.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/machine/timer.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/machine/timer.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/machine/translate.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/machine/translate.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/network/post.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/network/post.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/threads/alarm.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/threads/alarm.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/threads/kernel.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/threads/kernel.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/threads/main.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/threads/main.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/threads/scheduler.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/threads/scheduler.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/threads/synch.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/threads/synch.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/threads/synchlist.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/threads/synchlist.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/threads/thread.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/threads/thread.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/threads/threadtest.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/threads/threadtest.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/userprog/addrspace.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/userprog/addrspace.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/userprog/exception.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/userprog/exception.cc.o"
+  "/Users/qingqingluan/Desktop/nachos/code/userprog/synchconsole.cc" "/Users/qingqingluan/Desktop/nachos/cmake-build-debug/CMakeFiles/nachos.dir/code/userprog/synchconsole.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../c++example"
+  "../code"
+  "../code/build.cygwin"
+  "../code/build.linux"
+  "../code/build.macosx"
+  "../code/filesys"
+  "../code/lib"
+  "../code/machine"
+  "../code/network"
+  "../code/test"
+  "../code/threads"
+  "../code/userprog"
+  "../coff2noff"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
